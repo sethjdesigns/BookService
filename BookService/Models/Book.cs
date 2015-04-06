22d7fb1d3b4cf6b49ccc6a83b4a166617142dbd6
Seth Johnson <sethjdesigns@gmail.com>
2015-04-06 11:06:54 -0400

    added book class

---
+++ BookService/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public string Genre { get; set; }
+
+        //foreign key
+        public int AuthorId { get; set; }
+
+        //navgation property
+        public Author Author { get; set; }
+    }
+}
