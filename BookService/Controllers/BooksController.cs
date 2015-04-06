016f3b8a1148ccbeed4fc063b7dfc0b640baa101
Seth Johnson <sethjdesigns@gmail.com>
2015-04-06 13:41:09 -0400

    created Data Transfer Objects

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -18,17 +18,34 @@ namespace BookService.Controllers
         private BookServiceContext db = new BookServiceContext();
 
         // GET: api/Books
-        public IQueryable<Book> GetBooks()
+        public IQueryable<BookDTO> GetBooks()
         {
-            return db.Books
-                .Include(b => b.Author);
+            var books = from b in db.Books
+                        select new BookDTO()
+                        {
+                            Id = b.Id,
+                            Title = b.Title,
+                            AuthorName = b.Author.Name
+                        };
+
+            return books;
         }
 
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
+        [ResponseType(typeof(BookDetailDTO))]
         public async Task<IHttpActionResult> GetBook(int id)
         {
-            Book book = await db.Books.FindAsync(id);
+            var book = await db.Books.Include(b => b.Author).Select(b => 
+                new BookDetailDTO()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Year = b.Year,
+                    Price = b.Price,
+                    AuthorName = b.Author.Name,
+                    Genre = b.Genre
+                }).SingleOrDefaultAsync(b => b.Id == id);
+
             if (book == null)
             {
                 return NotFound();
@@ -50,7 +67,7 @@ namespace BookService.Controllers
             {
                 return BadRequest();
             }
-
+             
             db.Entry(book).State = EntityState.Modified;
 
             try
--- BookService/Models/BookServiceContext.cs
+++ BookService/Models/BookServiceContext.cs
@@ -24,4 +24,21 @@ namespace BookService.Models
         public System.Data.Entity.DbSet<BookService.Models.Book> Books { get; set; }
     
     }
+
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+    }
+
+    public class BookDetailDTO 
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string AuthorName { get; set; }
+        public string Genre { get; set; }
+    }
 }
