dbb2eb9cf0e8dcf3d99449af1f1cb6bc31cf29c5
Seth Johnson <sethjdesigns@gmail.com>
2015-04-13 09:59:59 -0400

    Added DbConfig.cs to get Database to update

--- BookService/Global.asax.cs
+++ BookService/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookService.App_Start;
+using BookService.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +15,7 @@ namespace BookService
     {
         protected void Application_Start()
         {
+            DbConfig.SeedIfNotEmpty(new BookServiceContext());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
